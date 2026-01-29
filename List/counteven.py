31e51d2923af482562856bc287ded5162a5b98ea
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-01-29 10:57:01 +0530

    Match Expression

---
+++ List/counteven.py
@@ -0,0 +1,6 @@
+list = [2,4,5,4,9,87,69]
+cnt = 0
+for num in list:
+    if num % 2 == 0:
+        cnt = cnt + 1
+print(cnt)
---
+++ Match-Expression/choice.py
@@ -0,0 +1,19 @@
+ch = int(input("Enter your choice: "))
+match ch:
+    case "1":
+        print("Tea")
+
+    case "2":
+        print("Coffee")
+
+    case "3":
+        print("Banana Shake")
+
+    case "4":
+        print("Burger")
+
+    case "5":
+        print("Butter Chicken with Garlic Butter Naan")
+
+    case _ :
+        print("Sorry Dear !")
