f662d6d67510844778cb167c05076b4cf59ceff2
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-01-31 10:00:19 +0530

    Tuple

---
+++ Tuple/tuple.py
@@ -0,0 +1,18 @@
+# Tuple access using index
+t = (10, 20, 30, 40, 50)
+
+print("First element:", t[0])
+print("Last element:", t[-1])
+print("Third element:", t[2])
+print("Third last element:", t[-3])
+
+t = (10, 50, 30)
+print(max(t))
+print(min(t))
+print(sum(t))
+print(len(t))
+print(sorted(t))
+print(tuple(reversed(t)))
+print(t.count(10))
+print(t.index(30))
+print("Accessing Tuple elements using indexing:")
