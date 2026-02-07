17adecd4c3f8e6d3c4b5b415a5ed223982eda657
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-07 10:00:02 +0530

    Set

---
+++ Set/Set.py
@@ -0,0 +1,7 @@
+s=set()
+print(type(s))
+set={1,2,"Hi",True,False,0}
+s.add(1)
+s.remove(1)
+s.add(684)
+print(s)
---
+++ Set/set1.py
@@ -0,0 +1,5 @@
+s=set((1,2,3))
+print(s)
+
+d=dict(name="Priyanshu Gupta",roll_no=513)
+print(d)
