0df64dbabee6581bc34c9410bd0e1ee73b132e1d
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-14 09:19:12 +0530

    Arguments

---
+++ Arguments/bank.py
@@ -0,0 +1,5 @@
+def bank(customer, bank ,accountnumber = 789456123500):
+    print(f"{customer} has a account in {bank} with the account number {accountnumber}")
+
+bank("Priyanshu Gupta", "State Bank Of India ")
+
--- Arguments/defaultargs.py
+++ Arguments/defaultargs.py
@@ -2,3 +2,4 @@ def book(title, author, price=500):
     print(f"{title} by {author} costs ₹{price}")
 
 book("Atomic Habits", "James Clear")
+
