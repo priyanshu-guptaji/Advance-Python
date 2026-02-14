d020729a731c5aebf1f8db6a72104635fdc15a6a
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-14 10:03:07 +0530

    Arguments

---
+++ Arguments/for.py
@@ -0,0 +1,16 @@
+def stu(*names):
+    for name in names:
+        print(f"Hi, my name is {name}.")
+
+stu(
+    "Priyanshu Gupta",
+    "Vinayak",
+    "Aarav Sharma",
+    "Aditya Verma",
+    "Rohan Mehta",
+    "Kunal Singh",
+    "Rahul Patel",
+    "Siddharth Jain",
+    "Ananya Reddy",
+    "Sneha Iyer"
+)
---
+++ Arguments/keyword.py
@@ -0,0 +1,4 @@
+def stu(*name):
+    print(f"Hi , my name is {name}.")
+
+stu("Priyanshu Gupta" , "Vinayak")
