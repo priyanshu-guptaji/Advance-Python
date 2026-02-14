8a4ef527ceecf618da0ae08877a0ad41ef2bbc0c
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-14 09:07:05 +0530

    Arguments

---
+++ Arguments/arg.py
@@ -0,0 +1,4 @@
+def stu(name , age , rollno , section):
+    print(f"Hello , Good Morning!!! \n My name is {name} , My Age is {age} , My Roll no. is {rollno} and I am from section {section}")
+
+stu("Priyanshu Gupta" , "20" , "23CSE513" , "I")
