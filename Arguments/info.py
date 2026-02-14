1dcfb95f419cb6c8a876341505fbc26be2d6d989
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-14 10:54:49 +0530

    Arguments

--- Arguments/info.py
+++ Arguments/info.py
@@ -12,3 +12,22 @@ info = {
 }
 
 student(info)
+
+students = [
+    {
+        "name": "Priyanshu Gupta",
+        "age": 20,
+        "blood group": "B+",
+        "city": "Delhi"
+    },
+    {
+        "name": "Vinayak Tripathy",
+        "age": 21,
+        "blood group": "O+",
+        "city": "Mumbai"
+    }
+]
+
+# Using for loop
+for info in students:
+    student(info)
