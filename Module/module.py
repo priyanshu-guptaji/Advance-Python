c01be0e3fb158aa8b1b89e08563dd8088fe06d34
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-24 11:01:44 +0530

    Module

--- Module/module.py
+++ Module/module.py
@@ -1,4 +1,5 @@
-import addition
-
-result = addition.add(10, 5)
-print(result)
+import calculator
+print(calculator.add(10, 5))
+print(calculator.sub(52,7))
+print(calculator.mul(52 ,78))
+print(calculator.div(49,7))
