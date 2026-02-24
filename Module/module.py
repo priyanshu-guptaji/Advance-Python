6c97549d230ecc51c84ef12cd74af1f1016b748f
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-02-24 11:08:40 +0530

    Module

--- Module/module.py
+++ Module/module.py
@@ -3,3 +3,11 @@ print(calculator.add(10, 5))
 print(calculator.sub(52,7))
 print(calculator.mul(52 ,78))
 print(calculator.div(49,7))
+
+print(dir(calculator))
+
+
+
+for name in dir(calculator):
+    if not name.startswith("__"):
+        print(name)
