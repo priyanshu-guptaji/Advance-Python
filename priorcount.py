2fd4f47a0754a93fb976e59b6419765d413668ef
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-06-08 11:11:36 +0530

    8th June

--- priorcount.py
+++ priorcount.py
@@ -1,9 +1,9 @@
 def count_elements(arr):
     l = arr[0]
-    cnt = 0
+    cnt = 1
 
-    for i in arr:
-        if i >= l:
+    for i in range(1, len(arr)):
+        if arr[i] >= l:
             cnt += 1
 
     return cnt
