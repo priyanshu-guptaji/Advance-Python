8a1768960c06d4b78a2e782f94a8aee434dfab79
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-06-09 16:45:12 +0530

    9th June

---
+++ monkey.py
@@ -0,0 +1,16 @@
+def monkeyleft(n,m,p,k):
+
+
+
+
+
+
+
+
+
+
+n = int(input("Total no of Monkeys"))
+p = int(input("Number of eatable Bananas by Single Monkey (Monkey that jumped down last may get less than k Bananas"))
+m = int(input("Number of eatable Peanuts by single Monkey(Monkey that jumped down last may get less than j Peanuts)"))
+k = int(input("Total number of Bananas"))
+j = int(input("Total number of Peanuts"))
