bfd578ee504d0e4b941aa8926108269e6b4f6f85
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-06-09 16:53:24 +0530

    9th June

--- monkey.py
+++ monkey.py
@@ -1,16 +1,24 @@
-def monkeyleft(n,m,p,k):
+def monkeyleft(n, m, p, k, j):
+
+    if n <= 0 or m < 0 or p < 0 or k <= 0 or j <= 0:
+        return "Invalid Input"
+
+    banana_monkeys = m // k
+    peanut_monkeys = p // j
+
+    fed = banana_monkeys + peanut_monkeys
+
+    if (m % k > 0) and (p % j > 0):
+        fed += 1
+
+    return n - fed
 
 
+n = int(input("Total number of monkeys: "))
+k = int(input("Bananas eaten by one monkey: "))
+j = int(input("Peanuts eaten by one monkey: "))
+m = int(input("Total bananas: "))
+p = int(input("Total peanuts: "))
 
-
-
-
-
-
-
-
-n = int(input("Total no of Monkeys"))
-p = int(input("Number of eatable Bananas by Single Monkey (Monkey that jumped down last may get less than k Bananas"))
-m = int(input("Number of eatable Peanuts by single Monkey(Monkey that jumped down last may get less than j Peanuts)"))
-k = int(input("Total number of Bananas"))
-j = int(input("Total number of Peanuts"))
+result = monkeyleft(n, m, p, k, j)
+print("Monkeys left on tree:", result)
